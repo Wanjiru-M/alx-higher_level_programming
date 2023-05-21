bc65d232c341f81704b3a95d78f5f9d08ebb0e9b
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-21 09:33:22 -0700

    Print 'object-oriented programming with Python' using slicing

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-string = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-
-new_string = string[39:66] + string[106:112] + string[:6]
-print(new_string)
-
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)

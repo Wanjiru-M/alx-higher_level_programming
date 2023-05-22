84b6980a6f0e7ba8b6ac313cef53717939aeb1d4
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-22 11:44:02 -0700

    a function that deletes the item at a specific position in a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
+    return None
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return my_list

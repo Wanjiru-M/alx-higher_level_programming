d48e115406909cece2d247872cba4b1c205c33e2
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-22 02:34:50 -0700

    print all possible different combinations of two digits in ascending order

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for num in range(9):
+for num in range(10):
     for digit in range(num + 1, 10):
-        print("{:02d}, {:02d}".format(num, digit), end=', ')
-print("{:02d}{:02d}".format(num + 1, digit))
+        if num < 9:
+            print("{:02d}, ".format(num), end='')
+        else:
+            print("{:02d}".format(num), end='')
+        print("{:02d}".format(digit))

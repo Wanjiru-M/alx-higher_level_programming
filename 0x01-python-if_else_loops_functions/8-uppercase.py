7afb7ed8bbefef9dea7ce44e44efb03e7fc6b541
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-22 02:48:55 -0700

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))

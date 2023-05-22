2c78bcbfa87f0fe9b7fc1a5898a234c49a65310d
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-22 09:32:07 -0700

    a program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    module_dir = dir(hidden_4)
-    visible_attributes = [item for item in module_dir if not item.startswith('__')]
-    for attribute in visible_attributes:
-        print(attribute)
+    import importlib.util
+    module_name = "hidden_4"
+    module_spec = importlib.util.find_spec(module_name)
+    if module_spec is None:
+        print(f"Module {module_name} not found.")
+    else:
+        module = importlib.util.module_from_spec(module_spec)
+        module_spec.loader.exec_module(module)
+        visible_attributes = [attribute for attribute in dir(module) if not attribute.startswith("__")]
+        for attribute in visible_attributes:
+            print(attribute)

bfaf55d39b77631b2fab0426928200bf355711d2
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-22 09:00:38 -0700

    a program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    if chr(i) in 'zyxwvutsrqponmlkjihgfedcba':
-        print(chr(i), end='')
-    if chr(i - 32) in 'ZYXWVUTSRQPONMLKJIHGFEDCBA':
-        print(chr(i - 32), end='')
+    print("{}".format(chr(i)), end='')
+    print(chr(i - 32), end='')

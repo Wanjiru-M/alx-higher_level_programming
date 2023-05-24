fb6e83ba3783baab736c757d49f061988c350dd2
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-24 02:55:02 -0700

    error fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,10 @@
 
 if __name__ == '__main__':
     import hidden_4
-    
-    visible_attributes = [attribute for attribute in dir(hidden_4) if not attribute.startswith('__')]
-    
+
+    visible_attributes = [
+        attribute for attribute in dir(hidden_4) if not attribute.startswith('__')
+    ]
+
     for attribute in visible_attributes:
         print(attribute)

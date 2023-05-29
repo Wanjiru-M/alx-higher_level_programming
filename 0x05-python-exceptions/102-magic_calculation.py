852d9790828b9da3e7a2589d2a6da85fcfcf41c5
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-29 08:09:32 -0700

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    try:
-        result = sum((a ** b) / i for i in range(1, 4) if i > a)
-    except:
-        result = b + a
+    result = 0
     return result

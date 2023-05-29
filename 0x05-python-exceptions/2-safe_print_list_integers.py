ac2f634246bd4ec0e32c771988d8b50676fb0883
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-29 02:37:56 -0700

    fixed trailing spaces

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    i = 0
-    def generator():
-        nonlocal i
-        while i < x:
-            try:
-                yield my_list[i]
-                i += 1
-            except IndexError:
-                break
-    for value in generator():
-        if isinstance(value, int):
-            print("{:d}".format(value), end=" ")
-            count += 1
+    integer_count = 0
+    for index in range(x):
+        try:
+            value = my_list[index]
+            print("{:d}".format(value), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            integer_count += 1
     print()
-    return count
+    return integer_count

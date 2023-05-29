aaa9b6c5a65b843b17e7f790ca85861dd95867e4
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-29 02:29:30 -0700

    fixed trailing spaces

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
-    except:
+        print("{:d}".format(value))
+    except(ValueError, TypeError):
         return False
+    else:
+        return True

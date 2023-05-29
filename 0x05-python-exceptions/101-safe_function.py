86d74f6d34a5d1f707d8a4e2eb4d4f92eaeafa54
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-29 07:53:31 -0700

    a function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 import sys
+
+def safe_function(function, *args):
+    try:
+        solution = function(*args)
+        return solution
+    except Exception as error:
+        error_message = f"Exception: {error!s}\n".format(**{"error": error})
+        sys.stderr.write(error_message)
+        solution = None
+
+    return solution

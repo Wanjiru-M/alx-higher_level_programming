e1f4772a22c72b17de4b5f14efdeaa45504267e3
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-08 07:44:00 -0700

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.set_width(width)
-        self.set_height(height)
+        self.width = width
+        self.height = height
 
-    @classmethod
-    def validate_value(cls, value, name):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value < 0:
-            raise ValueError(f"{name} must be >= 0")
-
-    def get_width(self):
+    @property
+    def width(self):
         return self.__width
 
-    def set_width(self, value):
-        self.validate_value(value, "width")
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
-    def get_height(self):
+    @property
+    def height(self):
         return self.__height
 
-    def set_height(self, value):
-        self.validate_value(value, "height")
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
-
-    width = property(get_width, set_width)
-    height = property(get_height, set_height)

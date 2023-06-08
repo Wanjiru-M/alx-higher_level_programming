3f45a46bcff559363ec20c0048a4b7f8a7492886
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-08 07:55:42 -0700

    task1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = 0
-        self.__height = 0
+        self._width = 0
+        self._height = 0
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
@@ -16,11 +16,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self._width = value
 
     @property
     def height(self):
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, value):
@@ -28,10 +28,10 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self._height = value
 
-    def __str__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
+    def area(self):
+        return self._width * self._height
 
-    def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
+    def perimeter(self):
+        return 2 * (self._width + self._height)

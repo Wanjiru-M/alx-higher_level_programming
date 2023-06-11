72bc6dddf26ac44b313a9c39016d27462249124f
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-11 10:29:14 -0700

    Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
 def magic_string():
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 1
-    else:
-        magic_string.count += 1
-    
-    string_list = []
-    for _ in range(magic_string.count):
-        string_list.append("BestSchool")
-    
-    return ", ".join(string_list)
+    if not hasattr(magic_string, "count"): magic_string.count = 1
+    else: magic_string.count += 1
+    return ", ".join(["BestSchool"] * magic_string.count)

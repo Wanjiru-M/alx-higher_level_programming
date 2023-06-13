2efb5b07a3708271bf6e707524d2da6c3e1c3284
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 09:24:52 -0700

    fix on checker

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Function that constructs a Python object from a JSON file."""
-import json
+import ast
+
 
 def load_from_json_file(filename):
     """Creates an object from a JSON file"""
     with open(filename) as file:
         json_str = file.read()
-    data = eval(json_str)
+    data = ast.literal_eval(json_str)
     return data
+

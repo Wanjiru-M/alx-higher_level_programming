bac092e938e85e21a5fafcdd4ed4186f273b8d1b
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 11:27:35 -0700

    search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,8 +4,7 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """A function that inserts a line of text"""
-     lines = []
-    with open(filename, 'r') as file:
+     with open(filename, 'r') as file:
         lines = file.readlines()
 
     with open(filename, 'w') as file:

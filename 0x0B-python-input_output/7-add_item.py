315c1bb4f844070d2cfc3608209910e3809dab32
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 09:35:30 -0700

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Incorporate all the command-line arguments into a Python list and persist the list by saving it to a file."""
 import sys
+
+
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
@@ -8,14 +10,9 @@ args = sys.argv[1:]
 filename = "add_item.json"
 
 try:
-    # Load existing data from the file
     data = load_from_json_file(filename)
 except FileNotFoundError:
     data = []
-
-# Add arguments to the data list using a for loop
 for arg in args:
     data.append(arg)
-
-# Save the updated data to the file
 save_to_json_file(data, filename)

a45700a54965bb650750a6ba3783c99e5ca92d59
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 11:47:00 -0700

    checker 7

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
-
+""" Creating Student class """
 
 class Student:
-    """Class representing a student"""
-    def __init__(self, first_name, last_name, age):
-        """Initialize a student instance"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    """ Definition of the Student class """
+    def init(self, first_name, last_name, age):
+    """ Initializes the Student instance """
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
 
-    def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of the student instance"""
-            return self.__dict__
-        json_dict = {}
-        for attr in attrs:
-            if hasattr(self, attr):
-                json_dict[attr] = getattr(self, attr)
-        return json_dict if json_dict else self.__dict__
+def to_json(self, attributes=None):
+    """ Retrieves a dictionary representation of the Student instance """
+    if isinstance(attributes, list) and all(isinstance(attr, str) for attr in attributes):
+        return {attr: getattr(self, attr) for attr in attributes if hasattr(self, attr)}
+    else:
+        return self.__dict__
+

4de4300bc934e4bbeb0f63096fb5f2435f1328c4
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 09:43:35 -0700

    fix on checker

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Script to add command-line arguments to a Python list and save them to a file."""
+"""Module 9-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
 
 import sys
 import json
 import os.path
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_func = import("5-save_to_json_file").save_to_json_file
+load_func = import("6-load_from_json_file").load_from_json_file
 
 file_name = 'add_item.json'
 
 my_list = []
 
 if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
-my_list = load_from_json_file(file_name)
+my_list = load_func(file_name)
 
 if len(sys.argv) > 1:
 for elem in sys.argv[1:]:
 my_list.append(elem)
 
-save_to_json_file(my_list, file_name)
+save_func(my_list, file_name)

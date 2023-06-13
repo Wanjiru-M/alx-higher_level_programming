23234058abc85c8c753700dfb49bdc488e55dad2
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 11:25:28 -0700

    search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """A function that adds a new line of text to an existing file"""
 
-import linecache
-import tempfile
-import shutil
 
 def append_after(filename="", search_string="", new_string=""):
-    temp_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
-    with open(filename, 'r') as file, temp_file:
-        line_number = 0
-        line = linecache.getline(filename, line_number)
-        while line:
-            temp_file.write(line)
-            if search_string in line:
-                temp_file.write(new_string)
-            line_number += 1
-            line = linecache.getline(filename, line_number)
+    """A function that inserts a line of text"""
+     lines = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
 
-    shutil.move(temp_file.name, filename)
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)

538c20f23155b2a1e665c52f7701022b6b1992db
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 08:46:46 -0700

    fix on checker

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-"""Retrieve content from a file"""
+"""Retrieve from a file"""
+
 
 def read_file(filename=""):
-"""Outputs the text content of a file to the standard output"""
-with open(filename) as file:
-text = file.read()
-print(text, end="")
+    """Reads text from a file and prints to stdoutput"""
+    with open(filename) as file:
+        text = file.read()
+    print(text,end="")

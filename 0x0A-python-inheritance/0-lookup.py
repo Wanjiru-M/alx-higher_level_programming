947c043a43c3baa1d2e42201b9987b26c5967c56
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 11:57:02 -0700

    My list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """ Creating a function"""
 
-
 def lookup(obj):
     """Returns a list of available attributes and methods of an object"""
-    attributes = []
-    for attr in dir(obj):
-        if not attr.startswith("__"):
-            attributes.append(attr)
-    return attributes
+    return dir(obj)

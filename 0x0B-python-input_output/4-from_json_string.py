9262a5406fcf94384040edc7b7a2ea6cbf7462bb
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 09:06:01 -0700

    json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""An object represented by a JSON string"""
+
+import json
+
+def from_json_string(my_str):
+    """Converts a JSON string into a Python data structure"""
+    return json.loads(my_str)

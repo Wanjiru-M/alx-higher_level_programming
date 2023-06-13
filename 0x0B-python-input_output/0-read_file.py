428ccaeba827e9f0c39b47c4aac6c4791c1ee374
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 03:18:30 -0700

    a function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,6 @@ def read_file(filename=""):
     """Reads a text file (UTF-8) and prints its contents to stdout"""
     try:
         with open(filename, encoding="utf-8") as file:
-            contents = file.read()
-            print(contents, end="")
+            print(file.read(), end="")
     except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-    except IOError:
-        print(f"Error: Unable to read file '{filename}'.")
+        return

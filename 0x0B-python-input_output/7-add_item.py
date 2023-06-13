b40a277aef7d077a72f66ffd23686dc14861e7c5
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 09:39:34 -0700

    fix on checker

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""Incorporate all the command-line arguments into a Python list and persist the list by saving it to a file."""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-if name == "main":
-save_to_json_file = import('5-save_to_json_file').save_to_json_file
-load_from_json_file = import('6-load_from_json_file').load_from_json_file
-
-try:
-    existing_items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    existing_items = []
-
-new_items = sys.argv[1:]
-
-items = existing_items + new_items
-
-save_to_json_file(items, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json") edit the variable names 

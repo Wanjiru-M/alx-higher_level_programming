f8f252ac3f6c3b29c62a8f178d6f40baa64f96a1
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 23:54:08 -0700

    fix on checker 13

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,10 +16,8 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-

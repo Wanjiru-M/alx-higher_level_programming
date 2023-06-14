41ce66ff183ab20c4d40602b2db9c26bce05f737
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 23:49:19 -0700

    ipmrove geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ creating a class """
 
-
-class BaseGeometry(object):
+class BaseGeometry:
     """Using the object class as the base class"""
-     def area(self):
+    def area(self):
         raise Exception("area() is not implemented")

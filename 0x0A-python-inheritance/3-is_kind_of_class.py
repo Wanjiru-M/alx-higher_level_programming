516ffba9248fce12cc6ddc1fd45673451e9b7fc1
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-13 23:33:33 -0700

    Traversing the object's class hierarchy

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,9 +4,4 @@
 
 def is_kind_of_class(obj, a_class):
     """Traversing the object's class hierarchy"""
-    current_class = type(obj)
-    while current_class != object:
-        if current_class == a_class:
-            return True
-        current_class = current_class.__base__
-    return False
+     return issubclass(type(obj), a_class)

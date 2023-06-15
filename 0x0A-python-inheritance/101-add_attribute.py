79189372b4fdb21419035a9a845c88c0e3c5e36e
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-15 02:10:16 -0700

    task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a function that adds attributes to objects."""
+    """Defines a function that adds attributes to objects."""
 
 def add_attribute(obj, attribute, value):
-"""adds a new attribute to an object if it's possible"""
-if isinstance(obj, dict):
-        obj[attribute] = value
+    """adds a new attribute to an object if it's possible"""
+    if isinstance(obj, dict):
+    obj[attribute] = value
     else:
-        setattr(obj, attribute, value)
+    setattr(obj, attribute, value)

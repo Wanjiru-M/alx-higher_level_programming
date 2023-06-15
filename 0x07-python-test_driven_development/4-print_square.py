8480bf43efdabf97780278580c897398aa3f1aad
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-15 02:38:34 -0700

    tasks

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+    """ say_my_name function."""
+
+    def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer or a float.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int) and not isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
+

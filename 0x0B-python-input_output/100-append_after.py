838da7adf1e617e9d95215b647d1c4cb351a71f5
wanjirumephis <wanjirunduhi@gmail.com>
2023-07-02 11:08:52 -0700

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-"""A function that adds a new line of text to an existing file"""
-
-import os
+"""A function that inserts a line of text"""
 
 
 def append_after(filename="", search_string="", new_string=""):
     """A function that inserts a line of text"""
-    temp_filename = filename + ".tmp"
-
-    with open(filename, 'r') as file, open(temp_filename, 'w') as temp_file:
-        for line in file:
-            temp_file.write(line)
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
             if search_string in line:
-                temp_file.write(new_string)
-
-    with open(temp_filename, 'r') as temp_file, open(filename, 'w') as file:
-        for line in temp_file:
-            file.write(line)
-
-    os.remove(temp_filename)
+                f.write(line + new_string)
+            else:
+                f.write(line)
